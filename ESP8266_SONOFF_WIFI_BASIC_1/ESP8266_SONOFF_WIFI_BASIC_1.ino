1e8eecbdf5c68151f66ade13f62d796d14b87c54
aformusatii <PIC12f67502!>
2020-11-01 01:06:20 +0200

    Added SONOFF WiFI Basic 1 Code

---
+++ ESP8266_SONOFF_WIFI_BASIC_1/ESP8266_SONOFF_WIFI_BASIC_1.ino
@@ -0,0 +1,175 @@
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h>
+#include <ESP8266WebServer.h>
+#include <ESP8266mDNS.h>
+#include <ESP8266HTTPUpdateServer.h>
+#include <WiFiManager.h>
+
+#define GPIO_POUT      12
+
+const char* host = "sonoff-wifi-basic-1";
+const char *ap_ssid = "sonoff-wifi-basic-1";
+
+ESP8266WebServer server(80);
+ESP8266HTTPUpdateServer httpUpdater;
+
+volatile bool out_port_status = false;
+
+void indexPage() {
+  String message = "<!doctype html>";
+  message += "<html lang=\"en\">";
+  message += "<head>";
+  message += "<title>ESP8266 SONOFF WiFi Basic 1</title>";
+  message += "</head>";
+  message += "<body>";
+  
+  message += "<form method='POST' action='/update' enctype='multipart/form-data'><input type='file' name='update'><input type='submit' value='Update'></form>";
+
+  message += "--------------------------------------------------------------<br/>";
+
+  message += "<a href=\"/switchOn\">switchOn</a><br/>";
+  message += "<a href=\"/switchOff\">switchOff</a><br/>";
+  message += "<a href=\"/switchToggle\">switchToggle</a><br/>";
+  message += "<a href=\"/status\">status</a><br/>";
+
+  uint32_t realSize = ESP.getFlashChipRealSize();
+  uint32_t ideSize = ESP.getFlashChipSize();
+  uint32_t chipId = ESP.getFlashChipId();
+  uint32_t chipSpeed = ESP.getFlashChipSpeed();
+  FlashMode_t ideMode = ESP.getFlashChipMode();
+
+  char buf[255];
+
+  message += "--------------------------------------------------------------<br/>";
+
+  sprintf(buf, "Flash chip id: %08X<br/>Flash chip speed: %u<br/>Flash chip size: %u<br/>Flash ide mode: %s<br/>Flash real size: %u<br/>",
+    chipId, 
+    chipSpeed,
+    ideSize,
+    (ideMode == FM_QIO ? "QIO" : ideMode == FM_QOUT ? "QOUT" : ideMode == FM_DIO ? "DIO" : ideMode == FM_DOUT ? "DOUT" : "UNKNOWN"),
+    realSize);
+
+  message += buf;
+  
+  message += "--------------------------------------------------------------<br/>";
+
+  /*
+    Signal Strength TL;DR   Required for
+    -30 dBm Amazing Max achievable signal strength. The client can only be a few feet from the AP to achieve this. Not typical or desirable in the real world.  N/A
+    -67 dBm Very Good Minimum signal strength for applications that require very reliable, timely delivery of data packets. VoIP/VoWiFi, streaming video
+    -70 dBm Okay  Minimum signal strength for reliable packet delivery. Email, web
+    -80 dBm Not Good  Minimum signal strength for basic connectivity. Packet delivery may be unreliable.  N/A
+    -90 dBm Unusable  Approaching or drowning in the noise floor. Any functionality is highly unlikely. N/A
+  */
+  message += "RSSI: ";
+  message.concat(WiFi.RSSI());
+  
+  message += "</body>";
+  
+  message += "</html>";
+  server.send(200, "text/html", message);
+}
+
+void handleNotFound() {
+  String message = "File Not Found\n\n";
+  message += "URI: ";
+  message += server.uri();
+  message += "\nMethod: ";
+  message += (server.method() == HTTP_GET)?"GET":"POST";
+  message += "\nArguments: ";
+  message += server.args();
+  message += "\n";
+  for (uint8_t i=0; i<server.args(); i++){
+    message += " " + server.argName(i) + ": " + server.arg(i) + "\n";
+  }
+  server.send(404, "text/plain", message);
+}
+
+void setupHTTPActions() {
+  server.on("/", indexPage);
+
+  server.on("/switchOn", [](){
+    server.send(200, "text/plain", "switch on");
+    setPortState(true);
+  });
+
+  server.on("/switchOff", [](){
+    server.send(200, "text/plain", "switch off");
+    setPortState(false);
+  });
+
+  server.on("/switchToggle", [](){
+    setPortState(!out_port_status);
+    if (out_port_status) {
+      server.send(200, "text/plain", "switch on"); 
+    } else {
+      server.send(200, "text/plain", "switch off"); 
+    }
+  });
+
+  server.on("/status", [](){
+    if (out_port_status) {
+      server.send(200, "text/plain", "switch on"); 
+    } else {
+      server.send(200, "text/plain", "switch off"); 
+    }
+  });
+
+  // Not found handler
+  server.onNotFound(handleNotFound);
+}
+
+void setPortState(bool state) {
+  out_port_status = state;
+
+  if (state) {
+    digitalWrite(GPIO_POUT, HIGH);
+  } else {
+    digitalWrite(GPIO_POUT, LOW);
+  }
+}
+
+void setupGPIO() {  
+  pinMode(GPIO_POUT, OUTPUT);
+  digitalWrite(GPIO_POUT, LOW);
+}
+
+void setup(void) {
+  setupGPIO();
+
+  setPortState(false);
+  
+  Serial.begin(115200);
+
+  // WiFi.mode(WIFI_STA);
+
+  delay(1000);
+  Serial.print("Connecting to WiFi");
+  delay(1000);
+
+  WiFiManager wifiManager;
+  wifiManager.setTimeout(120);
+
+  if (!wifiManager.autoConnect(ap_ssid)) {
+    Serial.println("Could not auto connect to WiFi"); 
+    delay(2000);
+  }
+
+  Serial.println("Connected to WiFi");
+  Serial.println(WiFi.localIP());
+
+  MDNS.begin(host);
+
+  setupHTTPActions();
+
+  httpUpdater.setup(&server);
+
+  server.begin();
+
+  MDNS.addService("http", "tcp", 80);
+}
+
+// ============= Main Loop =============
+void loop(void) {
+  server.handleClient();
+}
---
+++ ESP8266_SONOFF_WIFI_BASIC_1/README.md
@@ -0,0 +1,6 @@
+# Arduino Project for Sonoff WiFi Basic
+Modificated firmware.
+
+https://medium.com/@jeffreyroshan/flashing-a-custom-firmware-to-sonoff-wifi-switch-with-arduino-ide-402e5a2f77b
+
+https://github.com/kentaylor/EraseEsp8266Flash
